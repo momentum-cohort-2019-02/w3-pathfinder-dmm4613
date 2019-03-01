73993e38c8e3473192564ceeac0347bbb2af1340
Dan Miller <dmm4613@gmail.com>
2019-03-01 13:02:32 -0500

    added the pathfinder class that will map out the best path

--- draw_map.py
+++ draw_map.py
@@ -37,18 +37,64 @@ class Cartographer:
         for x in range(len(self.map.elevations[0])):
             for y in range(len(self.map.elevations)):
                 self.im.putpixel((x, y), (self.map.get_intensity(x, y), self.map.get_intensity(x, y), self.map.get_intensity(x, y)))        
-        self.im.save('elevation_map02.png')
+        self.im.save('elevation_map_find01.png')
 
 class Pathfinder:
     
-    def __init__(self):
-        pass
+    def __init__(self, map, filename, x=0, y=0):
+        self.map = map
+        self.im = Image.open(filename)
+        self.current_position = (x,y)
+    def get_elevation_difference (self, x):
+        """
+        by providing the current x coordinate, this method will find the difference in the three movement choices. 
+        """
+        y = self.current_position[1]
+        if y-1 < 0:
+            y = 0
+        current_elevation = self.map.get_elevation(x, y)
+        top_path = self.map.get_elevation((x+1),(y-1))
+        middle_path = self.map.get_elevation((x+1),y)
+        if y+1 > len(self.map.elevations):
+            y = len(self.map.elevations)-1
+        bottom_path = self.map.get_elevation((x+1),(y+1))
 
+        self.top_diff = abs(current_elevation - top_path)
+        self.middle_diff = abs(current_elevation - middle_path)
+        self.bottom_diff = abs(current_elevation - bottom_path)
+        self.choices = [self.top_diff, self.middle_diff, self.bottom_diff]
+        self.min_diff = min(self.choices)
+
+        # ###should probably split this part below into another method###
+      
+        if self.top_diff == self.min_diff and self.top_diff == self.bottom_diff and self.top_diff != self.middle_diff:
+            if random.randint(0,1) == 0:
+                y -= 1
+            else:
+                y += 1
+        elif self.top_diff == self.min_diff and self.top_diff != self.middle_diff:
+            y -= 1
+        elif self.bottom_diff == self.min_diff and self.bottom_diff != self.middle_diff:
+            y += 1
+        x =+ 1
+        self.current_position = (x,y)
+
+        return y
+        
+    def mark_my_trail(self):
+        """
+        The hiker will place a marker (pixel) on the current location by putpixel() on the drawn map.
+        """
+        y = self.current_position[1]
+        for x in range(len(self.map.elevations[0])-1):
+            self.im.putpixel((x,y), (0, 255, 0))
+            y = self.get_elevation_difference(x)
+        self.im.save('elevation_map_find01.png')
 
 if __name__ == "__main__":
-    
-    smallMap = Map('elevation_large.txt')
+   
+    smallMap = Map('elevation_small.txt')
     drawer = Cartographer(smallMap)
     drawer.draw_the_map()
-  
-
+    hiker = Pathfinder(smallMap, 'elevation_map_find01.png', y=280)
+    hiker.mark_my_trail()
